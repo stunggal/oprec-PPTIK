cf3c9d15fb424d98eed9b54c2b9dc6c7ed494e77
stunggal <54833890+stunggal@users.noreply.github.com>
2022-07-01 20:24:44 +0700

    Initial commit

---
+++ app/Http/Controllers/WeightController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\weight;
+use Illuminate\Http\Request;
+
+class WeightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\weight  $weight
+     * @return \Illuminate\Http\Response
+     */
+    public function show(weight $weight)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\weight  $weight
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(weight $weight)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\weight  $weight
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, weight $weight)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\weight  $weight
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(weight $weight)
+    {
+        //
+    }
+}
